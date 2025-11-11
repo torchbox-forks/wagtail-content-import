543ef4d309e2d4aaa8e7b5decc9170611e00f16c
nickmoreton <nickmoreton@me.com>
2025-11-11 12:03:14 +0000

    Use strict=True to fix ruff zip warning

--- src/wagtail_content_import/mappers/converters.py
+++ src/wagtail_content_import/mappers/converters.py
@@ -180,7 +180,7 @@ class ImageConverter(BaseConverter):
             # some other hashing scheme
             if potential_duplicate.file.size == image.file.size and all(
                 a == b
-                for a, b in zip(potential_duplicate.file.chunks(), image.file.chunks())
+                    for a, b in zip(potential_duplicate.file.chunks(), image.file.chunks(), strict=True)
             ):
                 # We've found an existing image in the library
                 # so let's not save our new image, and return this one instead
